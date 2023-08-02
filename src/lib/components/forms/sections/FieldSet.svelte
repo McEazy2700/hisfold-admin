0a41a8eaba8393be7be79e75b8ad348209b65937
Ezekiel Victor <codeepoch@gmail.com>
2023-08-02 02:51:04 +0100

    Restyled profile form for mobile

--- src/lib/components/forms/sections/FieldSet.svelte
+++ src/lib/components/forms/sections/FieldSet.svelte
@@ -2,7 +2,7 @@
   export let legend: string | null = null;
 </script>
 
-<fieldset class="flex mt-3 flex-col gap-1">
+<fieldset class="flex mt-3 flex-col gap-6 lg:gap-2">
   {#if legend}
     <legend class="opacity-30">{legend}</legend>
   {/if}
