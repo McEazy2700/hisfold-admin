653f7c7789372e7edce4ad229052aca16dc420d0
Ezekiel Victor <codeepoch@gmail.com>
2023-08-02 07:04:55 +0100

    preparing to add flowbite

--- src/lib/components/layoutSections/MobileDrawer.svelte
+++ src/lib/components/layoutSections/MobileDrawer.svelte
@@ -8,7 +8,7 @@
 		<h2 class="p-4 font-semibold text-lg">HisFold</h2>
 		<hr />
 		<div class="p-4 flex flex-col h-[87%] justify-between">
-			<div>
+			<div class="flex flex-col gap-1">
 				<NavUtils />
 				<Navigation />
 			</div>
