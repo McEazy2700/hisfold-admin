b60a602c25ac5120b3669082211fd99fc5ef8475
Ezekiel Victor <codeepoch@gmail.com>
2023-08-02 06:56:12 +0100

    Updated mobile drawer styles

--- src/lib/components/layoutSections/MobileDrawer.svelte
+++ src/lib/components/layoutSections/MobileDrawer.svelte
@@ -3,9 +3,9 @@
 	import { Navigation, NavUtils, User } from '$lib/components';
 </script>
 
-<Drawer>
-	<div class="h-full">
-		<h2 class="p-4 text-lg">HisFold</h2>
+<Drawer width="w-80">
+	<div class="h-full relative">
+		<h2 class="p-4 font-semibold text-lg">HisFold</h2>
 		<hr />
 		<div class="p-4 flex flex-col h-[87%] justify-between">
 			<div>
--- src/lib/components/navigation/NavUtils.svelte
+++ src/lib/components/navigation/NavUtils.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex flex-1 items-center gap-2">
 	<input bind:value={$SearchStore} type="search" class="input" placeholder="Search" />
-	<div class="absolute bottom-4 right-16 lg:static">
+	<div class="absolute bottom-5 right-4 lg:static">
 		<LightSwitch />
 	</div>
 </div>
