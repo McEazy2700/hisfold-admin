c3598e919c5d8fb702c28e61187d273cfd03f416
Ezekiel Victor <codeepoch@gmail.com>
2023-08-02 12:25:51 +0100

    Changed default logo

--- src/lib/stores/ProfileStore.ts
+++ src/lib/stores/ProfileStore.ts
@@ -13,7 +13,7 @@ interface Profile {
 }
 const ProfileStore = writable<Profile>({
   background: "https://plus.unsplash.com/premium_photo-1681077401356-bbe7057ec541?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80",
-  logo: "https://www.pngkey.com/png/full/208-2089134_rccg-logo-redeemed-christian-church-logo.png"
+  logo: "https://hisfold.com/h-nobg-1400w.png"
 })
 
 export default ProfileStore;
